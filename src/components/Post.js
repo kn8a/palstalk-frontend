abc9df645c89d48e706c3d943dd294fe0c674580
kn8a <88045655+kn8a@users.noreply.github.com>
2022-08-19 20:58:31 +0700

    add notes

--- src/components/Post.js
+++ src/components/Post.js
@@ -7,16 +7,22 @@ import axios from 'axios'
 import PostComments from './PostComments'
 
 function Post(props) {
+    //Props
     const userId = props.userId
     const token = props.token
+
+    //states
     const [post, setPost] = useState(props.post)
     const [like, setLike] = useState()
     const [commentsModal, setCommentsModal] = useState('')
+
+    //API URLS
     const postURL = `http://localhost:3000/api/posts/${post._id}`
     const likeURL = `http://localhost:3000/api/posts/${post._id}/like`
     const unlikeURL = `http://localhost:3000/api/posts/${post._id}/unlike`
-    const alreadyLiked = post.likes.indexOf(userId)
     
+    //check if user liked current post
+    const alreadyLiked = post.likes.indexOf(userId)
     useEffect(() => {
         if (alreadyLiked == -1) {
             setLike(<a onClick={likeFn}>Like</a>)
@@ -25,6 +31,7 @@ function Post(props) {
         }
     },[])
 
+    //pass to comment and updates post when a new comment is submitted
     const updatePost = () => {
         axios.get(postURL, {headers: {"Authorization": `Bearer ${token}`}})
             .then((response) => {
@@ -32,6 +39,7 @@ function Post(props) {
             })
     }
 
+    //like and switch like to unlike after click
     const likeFn = () => {
         axios.put(likeURL, '', {headers: {"Authorization": `Bearer ${token}`}})
         .then(() => {
@@ -40,9 +48,10 @@ function Post(props) {
                 setPost(response.data)
                 setLike(<a onClick={unlikeFn}>Unlike</a>)
             })
-            })
+        })
     }
 
+    //unlike and switch unlike to like after click
     const unlikeFn = () => {
         axios.put(unlikeURL, '', {headers: {"Authorization": `Bearer ${token}`}})
         .then(() => {
@@ -51,7 +60,7 @@ function Post(props) {
                 setPost(response.data)
                 setLike(<a onClick={likeFn}>Like</a>)
             })
-            })
+        })
     }
 
     const toggleCommentsModal = () => {
@@ -62,8 +71,6 @@ function Post(props) {
         }
         
     }
-
-//setLike(<a onClick={likeFn}>Like</a>)
 
   return (
     <div>
@@ -112,7 +119,6 @@ function Post(props) {
                     <PostComments postId={post._id} userId={userId} token={token} updatePost={updatePost}/>
                 </div>
             <button onClick={toggleCommentsModal} className="modal-close is-large" aria-label="close"></button>
-        
         </div>
     </div>
   )
