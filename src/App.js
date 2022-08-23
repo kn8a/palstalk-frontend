af0e71dba6f543db188476512559a8c59caf6494
kn8a <88045655+kn8a@users.noreply.github.com>
2022-08-23 17:53:44 +0700

    add Nav and icons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
+import Nav from './components/Nav';
 
 //pages import
 import Login from './pages/Login';
@@ -11,6 +12,7 @@ import Board from './pages/Board'
 import Profile from './pages/Profile'
 import Users from './pages/Users'
 import Register from './pages/Login'
+import Friends from './pages/Friends';
 
 
 function App() {
@@ -20,9 +22,14 @@ function App() {
   return (
     <div className='container is-max-desktop'>
     <Router>
+    <Nav/>
       <Routes>
         <Route path='/login' element={<Login/>} />
         <Route path='/' element={<Board/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/friends' element={<Friends/>} />
+        <Route path='/friends/sent' element={<Friends/>} />
+        <Route path='/friends/received' element={<Friends/>} />
       </Routes>
     </Router>
     <ToastContainer/>
