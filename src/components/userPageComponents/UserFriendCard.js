b34d3178517d4cc0c94ba739048bd2b8da748ad3
kn8a <88045655+kn8a@users.noreply.github.com>
2022-08-29 22:28:36 +0700

    my profile page

---
+++ src/components/userPageComponents/UserFriendCard.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {Title, Subtitle, Content, MediaLeft, MediaContent, Image, CardContent, CardFooterItem, card, Section, Column, Columns, Modal, ModalBackground, ModalContent, ModalClose} from 'bloomer'
+import { Loader, Card, Media, Block, Button, Heading  } from 'react-bulma-components'
+import { Container } from 'bloomer/lib/layout/Container'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { Link, useNavigate } from 'react-router-dom'
+
+function UserFriendCard(props) {
+  
+  const token = localStorage.getItem('palstalkToken')
+  const userId = localStorage.getItem('palstalkUserId')
+  const updateFriends = props.updateFriends
+
+
+  return (
+    <Card style={{ width: 200, margin: 'auto' }} textAlign='center'>
+      
+      <Card.Content>
+        <Media justifyContent='center'>
+          <Media.Item renderAs="figure" align="left">
+            <Image
+              isSize="96x96"
+              alt={`Picture of ${props.friend.name_first} ${props.friend.name_last}`}
+              src={props.friend.profile_pic}
+            />
+          </Media.Item>
+          
+        </Media>
+        <Container>
+            <Link to={`/users/${props.friend._id}`}>
+            <Heading size={6}>{`${props.friend.name_first} ${props.friend.name_last}`}</Heading>
+            </Link>
+          </Container>
+          <Block></Block>
+        
+      </Card.Content>
+    </Card>
+  )
+}
+
+export default UserFriendCard
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,241 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Button, Box, Message, Control, Label, Input, Textarea, Help, Form, Block, Modal, Container } from 'react-bulma-components'
+
 
 function Profile() {
+
+  const profilePic = localStorage.getItem('palstalkUserPic')
+  const profileName = localStorage.getItem('palstalkUserName')
+  const token = localStorage.getItem('palstalkToken')
+  const ProfileURL = `http://localhost:3000/api/users/profile`
+
+  const [user,setUser] = useState({
+    name_first: '',
+    name_last: '',
+    gender: '',
+    email: '',
+    bio:'',
+    location: '',
+    password: '',
+    profile_pic: '',
+    friends: [],
+    pending_requests: [],
+    posts: [],
+    role: '',
+  })
+
+  const [userAsPulled,setUserAsPulled] = useState({
+    name_first: '',
+    name_last: '',
+    gender: '',
+    email: '',
+    bio:'',
+    location: '',
+    password: '',
+    profile_pic: '',
+    friends: [],
+    pending_requests: [],
+    posts: [],
+    role: '',
+  })
+
+  const [editModal, setEditModal] = useState(null)
+
+  useEffect(()=> {
+    axios.get(ProfileURL, {headers: {"Authorization": `Bearer ${token}`}})
+    .then((response) => {
+      setUser(response.data)
+      setUserAsPulled(response.data)
+    })
+  },[])
+
+  const toggleEditModal = () => {
+    if (!editModal) {
+        setEditModal('is-active')
+    } else {
+        setEditModal(null)
+    }
+    
+}
+
+  const cancelEdit = () => {
+    setUser(userAsPulled)
+    toggleEditModal()
+  }
+
+  const updateProfile = () => {
+    
+  }
+
+
   return (
-    <div>Profile</div>
+    <div>
+      <Block></Block>
+          
+          <Button onClick={toggleEditModal}>Edit Profile</Button>
+          
+          <div className={`modal ${editModal}`}>
+            <div className="modal-background" onClick={toggleEditModal}></div>
+                <div className="modal-content">
+                <Box>
+
+                    <Form.Field horizontal>
+                      <Form.Field.Label>
+                        <Form.Label>
+                          First name:
+                        </Form.Label>
+                      </Form.Field.Label>
+                      <Form.Field.Body>
+                        <Form.Field>
+                          <Form.Control>
+                            <Form.Input
+                              placeholder="Field control - text input"
+                              type="text"
+                              value={user.name_first}
+                            />
+                          </Form.Control>
+                          
+                          
+                        </Form.Field>
+                      </Form.Field.Body>
+                    </Form.Field>
+                    <Form.Field horizontal>
+                      <Form.Field.Label>
+                        <Form.Label>
+                          Last name:
+                        </Form.Label>
+                      </Form.Field.Label>
+                      <Form.Field.Body>
+                        <Form.Field>
+                          <Form.Control>
+                            <Form.Input
+                              type="text"
+                              value={user.name_last}
+                            />
+                          </Form.Control>
+                          
+                          
+                        </Form.Field>
+                      </Form.Field.Body>
+                    </Form.Field>
+
+                    
+
+                    <Form.Field horizontal>
+                      <Form.Field.Label>
+                        <Form.Label>
+                          Location:
+                        </Form.Label>
+                      </Form.Field.Label>
+                      <Form.Field.Body>
+                        <Form.Field>
+                          <Form.Control>
+                            <Form.Input
+                              placeholder="City, Country"
+                              type="text"
+                              value={user.location}
+                            />
+                          </Form.Control>
+                          
+                          <Form.Help>
+                            You can also include a state and Postal code
+                          </Form.Help>
+                        </Form.Field>
+                      </Form.Field.Body>
+                    </Form.Field>
+                    <Form.Field horizontal>
+                      <Form.Field.Label>
+                        <Form.Label>
+                          Bio:
+                        </Form.Label>
+                      </Form.Field.Label>
+                      <Form.Field.Body>
+                        <Form.Field>
+                          <Form.Control>
+                            <Form.Textarea
+                              value={user.bio}
+                              placeholder=""
+                              type="text"
+                            />
+                          </Form.Control>
+                        </Form.Field>
+                      </Form.Field.Body>
+                    </Form.Field>
+
+                    <Form.Field horizontal>
+                      <Form.Field.Label>
+                        <Form.Label>
+                          Gender:
+                        </Form.Label>
+                      </Form.Field.Label>
+                      <Form.Field.Body>
+                        <Form.Field>
+                          <Form.Control>
+                            <Form.Select defaultValue={user.gender}>
+                              <option>Male</option>
+                              <option>Female</option>
+                              <option>Other</option>
+                              <option>Prefer not to say</option>
+                            </Form.Select>
+                          </Form.Control>
+                        </Form.Field>
+                      </Form.Field.Body>
+                    </Form.Field>
+                    <Block></Block>
+                    <Container display='flex' justifyContent='space-around'>
+                    <Button color={'success'} onClick={updateProfile}>Save</Button>
+                    <Button color={'danger'} onClick={cancelEdit}>Cancel</Button>
+                    </Container>
+                    
+
+                    </Box>
+
+                </div>
+            <button onClick={toggleEditModal} className="modal-close is-large" aria-label="close"></button>
+        </div>
+          
+        <Box>
+
+<Form.Field horizontal>
+  <Form.Field.Label>
+    <Form.Label>
+      Profile Pic:
+    </Form.Label>
+  </Form.Field.Label>
+  <Form.Field.Body>
+    <Form.Field>
+      <Form.Control>
+        <Form.InputFile
+          
+        />
+      </Form.Control>
+    </Form.Field>
+  </Form.Field.Body>
+</Form.Field>
+
+
+
+<Form.Field horizontal>
+  <Form.Field.Label>
+    <Form.Label>
+      Pic URL:
+    </Form.Label>
+  </Form.Field.Label>
+  <Form.Field.Body>
+    <Form.Field>
+      <Form.Control>
+        <Form.Input
+          
+        />
+      </Form.Control>
+    </Form.Field>
+  </Form.Field.Body>
+</Form.Field>
+
+</Box>
+          
+    </div>
   )
 }
 
