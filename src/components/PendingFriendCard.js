39fa3522785c20cada6afd514fca11857fe52b6d
kn8a <88045655+kn8a@users.noreply.github.com>
2022-09-07 15:34:17 +0700

    remove bloomer components

--- src/components/PendingFriendCard.js
+++ src/components/PendingFriendCard.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import {Title, Subtitle, Content, MediaLeft, MediaContent, Image, CardContent, CardFooterItem, card, Section, Column, Columns, Modal, ModalBackground, ModalContent, ModalClose} from 'bloomer'
-import { Loader, Card, Media, Block, Button, Heading  } from 'react-bulma-components'
+
+import { Loader, Card, Media, Block, Button, Heading, Image, Content,  } from 'react-bulma-components'
 import { Container } from 'bloomer/lib/layout/Container'
 import axios from 'axios'
 import { toast } from 'react-toastify'
@@ -15,7 +15,7 @@ function PendingFriendCard(props) {
         <Media justifyContent='center'>
           <Media.Item display='flex' justifyContent='center' renderAs="figure">
             <Image
-              isSize="128x128"
+              size={128}
               alt={`Picture of ${props.user.name_first} ${props.user.name_last}`}
               src={`http://localhost:3000/api/file/${props.user.profile_pic}`}
             />
