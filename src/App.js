ef1e8e3d9c086a7644b1bedf89d4f0e993e8e62f
kn8a <88045655+kn8a@users.noreply.github.com>
2022-09-07 15:44:58 +0700

    remove bloomer components

--- src/App.js
+++ src/App.js
@@ -5,20 +5,18 @@ import 'react-toastify/dist/ReactToastify.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
 import Nav from './components/Nav';
-import { ChakraProvider } from '@chakra-ui/react'
 
 //pages import
 import Login from './pages/Login';
 import Board from './pages/Board'
 import Profile from './pages/Profile'
-import Users from './pages/Users'
-import Register from './pages/Login'
+
 import Friends from './pages/Friends';
 import FindFriends from './pages/FindFriends';
 import ReceivedRequests from './pages/ReceivedRequests'
 import SentRequests from './pages/SentRequests';
 import UserPage from './pages/UserPage';
-import Login2 from './pages/Login2';
+
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
@@ -53,7 +51,6 @@ function App() {
     <Nav/>
       <Routes>
         <Route path='/login' element={<Login/>} />
-        <Route path='/login2' element={<Login2/>} />
         <Route path='/' element={<Board/>} />
         <Route path='/profile' element={<Profile/>} />
         <Route path='/friends' element={<Friends/>} />
